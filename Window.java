7b42eaf5848d071ed87ec5d1730a1b190a1bc40a
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-22 13:23:24 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,26 +1,19 @@
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-public class Window(){
-    private static void createwindow(){
-      //set up window
-        JFrame frame =  new JFrame("Hello World App");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel textlabel = new JLabel("Hello World App", swingConstants.CENTER);
-        textlabel.setPreferredSize(new Dimension(300, 100));
-        frame.getcontentPane().add(textlabel, BorderLayout.CENTER);
-        //Display the window
-        frame.setLocationRelativeTo(null);
-        frame.pack();
-        frame.setVisible(true); 
-    }
-    //create popup message that says Hello World
-    public class MessagePopUps {
-      public static void main(String[] a){
-        JFrame parent = new JFrame();
-        JOptionPane.showMessageDialog(parent, "Hello World");
-      }
-    }
-    public static void main(String[] args) {
-        createwindow();
-    }
+import javax.swing.JButton;
+public class Window {
+    final JFrame parent = new JFrame();
+    JButton button = new JButton();
+    button.setText("Click me to show message dialog");
+    parent.add(button);
+    parent.pack();
+    parent.setVisible(true);
+  
+    button.addActionListener(new java.awt.event.ActionListener(){
+        @Override
+        public void actionPerformed(java.awt.event.ActionEvent evt){
+            String name =  JOptionPane.showInputDialog(parent, "Hello World", null);
+        }
+    });
+  }              
 }
