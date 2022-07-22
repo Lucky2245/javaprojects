7b7879031ebae8110f515d9e14102eaafc5902c7
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-22 00:28:55 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,20 +1,19 @@
 import java.awt.*;
 import javax.swing.*;
-
 public class Window(){
     private static void createwindow(){
       //set up window
-      JFrame frame =  new JFrame("Simple Window");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      JLabel textlabel = new JLabel("Welcome To My App", swingConstants.CENTER);
-      textlabel.setPreferredSize(new Dimension(300, 100));
-      frame.getcontentPane().add(textlabel, BorderLayout.CENTER);
-      //Display the window
-      frame.setLocationRelativeTo(null);
-      frame.pack();
-      frame.setVisible(true);
+        JFrame frame =  new JFrame("Simple Window");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel textlabel = new JLabel("Welcome To My App", swingConstants.CENTER);
+        textlabel.setPreferredSize(new Dimension(300, 100));
+        frame.getcontentPane().add(textlabel, BorderLayout.CENTER);
+        //Display the window
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.setVisible(true); 
     }
-  public static void main(String[] args) {
-    createwindow();
-  }
+    public static void main(String[] args) {
+        createwindow();
+    }
 }
