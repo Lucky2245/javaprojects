08fac7132a3eecb59b26713fdd0860af6454c9da
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-21 21:40:47 -0400

    Update Main.js

--- program1/jsandjava/Main.js
+++ program1/jsandjava/Main.js
@@ -1,1 +1,5 @@
-document.write("Hello World!");
+var header = document.createElement("h1");
+header.innerText = "Hello World!");
+header.id = "headerone";
+document.getElemebtById("headerone").style.textAlign = "center";
+document.body.appendChild(header);
