4e8b7f0ac5488aa94d2f80cc822e951778140d22
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-22 12:25:35 -0400

    Create PopupMessage.java

---
+++ PopupMessage.java
@@ -0,0 +1,6 @@
+public class MessagePopUps {
+      public static void main(String[] a){
+        JFrame parent = new JFrame();
+        JOptionPane.showMessageDialog(parent, "Hello World");
+      }
+}
