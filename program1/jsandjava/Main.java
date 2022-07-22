ee782962f7be7e1c371f44b922544d09cdb5a5da
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-21 21:32:26 -0400

    Update Main.java

--- program1/jsandjava/Main.java
+++ program1/jsandjava/Main.java
@@ -5,7 +5,7 @@ import java.io.util.Scanner;
 public class ReadFile {
   public static void main(String[] args) {
     try{ 
-        File myObj = new File("js.js");
+        File myObj = new File("Main.js");
         Scanner myReader = new Scanner(myObj);
         while(myReader.hasNextLine()) {
           String data = myReader.nextLine():
