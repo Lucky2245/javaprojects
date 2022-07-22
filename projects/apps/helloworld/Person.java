cce30d0cdfe579432b86e852bfc8f1b89ca192ff
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-22 16:45:23 -0400

    Create Person.java

---
+++ projects/apps/helloworld/Person.java
@@ -0,0 +1,12 @@
+public class Person{
+	        public String fname = "John";
+	        public String lname = "doe";
+	        public int age = 20;
+	           }
+public class Program
+{
+    public static void main(String[] args){
+	        Person person = new Person();
+	        System.out.println("My Name Is:" + person.fname + " and my last name is:" + person.lname + " And my age is:" + person.age);
+	    }
+}
