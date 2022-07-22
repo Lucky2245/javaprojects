4d8fa2d3d82ac28eeafb94f5891b59e2a5d90b29
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-22 12:30:39 -0400

    Delete PopupMessage.java

--- PopupMessage.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-public class MessagePopUps {
-      public static void main(String[] a){
-        JFrame parent = new JFrame();
-        JOptionPane.showMessageDialog(parent, "Hello World");
-      }
-}
