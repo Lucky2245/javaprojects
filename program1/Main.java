7bdd404a94854e53f9159bad7e71138bdf708406
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-07-21 20:09:33 -0400

    Update Main.java

--- program1/Main.java
+++ program1/Main.java
@@ -1,1 +1,24 @@
-public class Main {}
+public class Main {
+  public static void main(String args) {
+    System.out.println("Hello World!");
+    System.out.println("I am learning Java again.");
+    System.out.println("it is awesome!");
+    System.out.println(3+3);
+    //print Hello World! 
+    System.out.println("Hello World!");
+    /* The code bellow will print the words Hello World
+    to the screen, and it is amazing */
+    System.out.println("Hello World");
+    int myNum = 5; //Integer (whole number)
+    float myFloatNum = 5.99f; // Floating point number
+    char MyLetter = "D" // Character
+    boolean mybool = true; // Boolean
+    String myText = "Hello"; // String
+    byte myNumber = 100;/*The byte data type can store whole numbers from -128 to 127. This can be used instead of int or other integer types to save memory when you are certain that the value will be within -128 and 127:*/
+    System.out.println(myNumber);
+    short myNumb = 5000; /*The short data type can store whole numbers from -32768 to 32767*/
+    System.out.println(myNumb);
+    int mynumber = 10;/*The int data type can store whole numbers from -2147483648 to 2147483647. In general, and in our tutorial, the int data type is the preferred data type when we create variables with a numeric value.*/
+    System.out.println(mynumber);
+  }
+}
